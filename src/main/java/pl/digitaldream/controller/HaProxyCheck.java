83e3dc4101097376541c64f6ce678a8a385db265
Witek <baranowskiwitek@gmail.com>
2014-11-06 00:23:42 +0100

    health check

---
+++ src/main/java/pl/digitaldream/controller/HaProxyCheck.java
@@ -0,0 +1,19 @@
+package pl.digitaldream.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@Controller
+public class HaProxyCheck {
+
+    @RequestMapping(value = "/health", method = RequestMethod.HEAD)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void health(){}
+	
+
+}
